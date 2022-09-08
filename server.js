7074a9e921d6601abb7b788d113fe9877a87e4b1
Evan Parke <eparke@DESKTOP-IF9RP0H.localdomain>
2022-09-07 20:51:52 -0400

    Modified how I handled the ports.

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const fs = require('fs');
 // Require minimist module (make sure you install this one via npm).
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-
+const  args = require('minimist')(process.argv.slice(2));
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = require('minimist')(process.argv.slice(2)) || 3000;
+const port = args.port || 3000;
 if(isNaN(port.port) || port.port < 0 || port.port > 65536 ){
 	port.port = 3000}
 else{
@@ -52,8 +52,8 @@ const server = http.createServer((req, res) => {
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
-server.listen(port.port,() => {
-	  console.log(`Server listening on port ${port.port}`)
+server.listen(port,() => {
+	  console.log(`Server listening on port ${port}`)
 	})
 
 
